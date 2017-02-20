585f406614449aeacdee26ab064de3ba1cd7184e
tintoy <tintoy@tintoy.io>
2017-02-20 14:25:53 +1100

    No need to force qualification of element names.

--- src/PSReptile/Models/Maml/Command.cs
+++ src/PSReptile/Models/Maml/Command.cs
@@ -13,37 +13,37 @@ namespace PSReptile.Models.Maml
         /// <summary>
         ///     The command details.
         /// </summary>
-        [XmlElement("details", Namespace = Constants.XmlNamespace.Command, Form = XmlSchemaForm.Qualified, Order = 0)]
+        [XmlElement("details", Namespace = Constants.XmlNamespace.Command, Order = 0)]
         public CommandDetails Details { get; set; } = new CommandDetails();
 
         /// <summary>
         ///     The command's detailed description (one or more paragraphs; "maml:description/maml:para").
         /// </summary>
-        [XmlArray("description", Namespace = Constants.XmlNamespace.MAML, Form = XmlSchemaForm.Qualified, Order = 1)]
-        [XmlArrayItem("para", Namespace = Constants.XmlNamespace.MAML, Form = XmlSchemaForm.Qualified)]
+        [XmlArray("description", Namespace = Constants.XmlNamespace.MAML, Order = 1)]
+        [XmlArrayItem("para", Namespace = Constants.XmlNamespace.MAML)]
         public List<string> Description { get; set; } = new List<string>();
 
         // TODO: create types for the elements below.
         
-        [XmlElement("syntax", Namespace = Constants.XmlNamespace.Command, Form = XmlSchemaForm.Qualified, Order = 2)]
+        [XmlElement("syntax", Namespace = Constants.XmlNamespace.Command, Order = 2)]
         public string Syntax { get; set; }
 
-        [XmlElement("parameters", Namespace = Constants.XmlNamespace.Command, Form = XmlSchemaForm.Qualified, Order = 3)]
+        [XmlElement("parameters", Namespace = Constants.XmlNamespace.Command, Order = 3)]
         public string Parameters { get; set; }
 
-        [XmlElement("inputTypes", Namespace = Constants.XmlNamespace.Command, Form = XmlSchemaForm.Qualified, Order = 4)]
+        [XmlElement("inputTypes", Namespace = Constants.XmlNamespace.Command, Order = 4)]
         public string InputTypes { get; set; }
 
-        [XmlElement("returnValues", Namespace = Constants.XmlNamespace.Command, Form = XmlSchemaForm.Qualified, Order = 5)]
+        [XmlElement("returnValues", Namespace = Constants.XmlNamespace.Command, Order = 5)]
         public string ReturnValues { get; set; }
 
-        [XmlElement("alertSet", Namespace = Constants.XmlNamespace.MAML, Form = XmlSchemaForm.Qualified, Order = 6)]
+        [XmlElement("alertSet", Namespace = Constants.XmlNamespace.MAML, Order = 6)]
         public string AlertSet { get; set; }
 
-        [XmlElement("examples", Namespace = Constants.XmlNamespace.Command, Form = XmlSchemaForm.Qualified, Order = 7)]
+        [XmlElement("examples", Namespace = Constants.XmlNamespace.Command, Order = 7)]
         public string Examples { get; set; }
 
-        [XmlElement("relatedLinks", Namespace = Constants.XmlNamespace.MAML, Form = XmlSchemaForm.Qualified, Order = 8)]
+        [XmlElement("relatedLinks", Namespace = Constants.XmlNamespace.MAML, Order = 8)]
         public string RelatedLinks { get; set; }
     }
 }
