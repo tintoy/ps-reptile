f9da18fd150aa7b7585d1cf6037ab1b03b65214f
tintoy <tintoy@tintoy.io>
2017-02-20 14:24:23 +1100

    Verify generated XML.

--- test/PSReptile.Tests/XmlTests.cs
+++ test/PSReptile.Tests/XmlTests.cs
@@ -18,9 +18,28 @@ namespace PSReptile.Tests
         [Fact]
         public void GenerateXml_From_Command()
         {
+            string expected = @"
+<?xml version=""1.0"" encoding=""utf-16""?>
+<command:command xmlns:maml=""http://schemas.microsoft.com/maml/2004/10"" xmlns:dev=""http://schemas.microsoft.com/maml/dev/2004/10"" xmlns:command=""http://schemas.microsoft.com/maml/dev/command/2004/10"">
+  <command:details>
+    <command:name>Get-FooBar</command:name>
+    <command:verb>Get</command:verb>
+    <command:noun>FooBar</command:noun>
+    <maml:description>
+      <maml:para>Retrieve one or more FooBars.</maml:para>
+    </maml:description>
+  </command:details>
+  <maml:description>
+    <maml:para>This command works with FooBars.</maml:para>
+    <maml:para>It gets them.</maml:para>
+    <maml:para>I don't really know how to make it any clearer than that.</maml:para>
+  </maml:description>
+</command:command>
+            ".Trim();
+
             XmlSerializerNamespaces namespacesWithPrefix = Constants.XmlNamespace.GetStandardPrefixes();
             XmlSerializer serializer = new XmlSerializer(typeof(Command));
-            
+
             string actual;
             using (StringWriter writer = new StringWriter())
             {
@@ -51,8 +70,8 @@ namespace PSReptile.Tests
 
                 actual = writer.ToString();
             }
-            
-            Console.WriteLine(actual);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
